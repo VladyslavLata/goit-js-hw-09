e6500ae6071e2f01aecfbb3da145d534160c97c0
VladyslavLata <vladlatahtc@gmail.com>
2022-06-14 14:54:05 +0300

    1 task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.body;
+let timerChangeBgColor = null;
+btnStartEl.addEventListener('click', onBtnClickChangeBgColor);
+btnStopEl.addEventListener('click', onBtnClickStopChangeBgColor);
 
+btnStopEl.setAttribute('disabled', '');
+
+function onBtnClickChangeBgColor() {
+  timerChangeBgColor = setInterval(changeBgColor, 1000);
+  btnStartEl.setAttribute('disabled', '');
+  btnStopEl.removeAttribute('disabled');
+}
+
+function onBtnClickStopChangeBgColor() {
+  clearInterval(timerChangeBgColor);
+  btnStopEl.setAttribute('disabled', '');
+  btnStartEl.removeAttribute('disabled');
+}
+
+function changeBgColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
