ce73770170b14e691a1b97b1b99227069819078b
VladyslavLata <vladlatahtc@gmail.com>
2022-06-16 15:27:51 +0300

    1 and 3 task completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+const formEl = document.querySelector('.form');
+const btnCreatePromiseEl = document.querySelector('button[type=submit]');
+btnCreatePromiseEl.addEventListener('click', onClickBtnCreatePromise);
+formEl.addEventListener('input', onInputValue);
+let formData = {};
+
+function onInputValue(e) {
+  formData[e.target.name] = e.target.value;
+}
+
+function onClickBtnCreatePromise() {
+  event.preventDefault();
+  let delay = Number(formData.delay);
+
+  for (let i = 1; i <= formData.amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formData.step);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
